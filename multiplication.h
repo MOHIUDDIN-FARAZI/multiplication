3cd7510e76b2a47a9d5acffeb981d8db705a51d1
MOHIUDDIN-FARAZI <m.farazi2003@gmail.com>
2025-11-30 22:33:09 +0600

    Add multiplication header with function declarations

---
+++ multiplication.h
@@ -0,0 +1,8 @@
+#ifndef MULTIPLICATION_H
+#define MULTIPLICATION_H
+
+int multiply(int a, int b);
+float multiply_float(float a, float b);
+void print_multiplication_table(int number, int limit);
+
+#endif
